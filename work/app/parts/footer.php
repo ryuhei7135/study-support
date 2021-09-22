13d914b479c9f80bedc9ea5d811fcf30a4965128
ryuhei7135 <79356513+ryuhei7135@users.noreply.github.com>
2021-09-22 21:41:53 +0900

    Delete work directory

--- work/app/parts/footer.php
+++
@@ -1,9 +0,0 @@
-    <script src="js/main.js?p=(new Date()).getTime()"></script>
-    <script src="js/Class.js/Content.js"></script>
-    <script src="js/Class.js/Folder.js"></script>
-    <script src="js/Class.js/Modal.js"></script>
-    <script src="js/Class.js/Record.js"></script>
-    <script src="js/Class.js/Utils.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-</body>
-</html>
--- work/public/js/Class.js/Utils.js
+++
@@ -1,7 +0,0 @@
-class Utils {
-  //ブラウザバックボタン押下時
-  static clickBackBtn() {
-    history.pushState(null, null, null);
-    window.alert("ブラウザの戻るボタンは使わないでください");
-  }
-}
